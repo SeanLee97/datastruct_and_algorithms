7eeb82a742332210c5636ef4f58d3b49396c5fdf
SeanLee97 <mitree@sina.com>
2018-04-09 21:49:25 +0800

    add algo

--- sort_search/heap_sort.py
+++ sort_search/heap_sort.py
@@ -1,4 +1,38 @@
 # !/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-def heap_sort()
+# 堆排序
+def max_heap(lst, n, i):
+    # i为当前根节点
+    largest = i # 初始化最大值
+    L = 2*i + 1 # 左子树
+    R = 2*i + 2 # 右子树
+    # 判断左子树是否存在，且是否大于根
+    if L < n and  lst[L] > lst[i]:
+        largest = L
+    # 判断右子树是否存在，且是否大于根
+    if R < n and lst[R] > lst[largest]:
+        largest = R
+    # 如果最大值的下标不等于当前根的下标那么交换两者的值
+    if largest != i:
+        lst[i], lst[largest] = lst[largest], lst[i]
+        # 由于一次调整后，堆仍然违反堆性质，所以需要递归的测试，使得整个堆都满足堆性质
+        max_heap(lst, n, largest)
+
+def heap_sort(lst):
+    n = len(lst) -1
+    # 建立大顶堆
+    # 作用是将一个数组改造成一个最大堆，为了保证下标i的结点之后的结点都能满足最大堆的性质，采用自下而上（数组中从后往前）的调用max_heap()函数来往上构建大顶堆
+    for i in range(n, -1, -1):
+        max_heap(lst, n, i)
+    
+    # 从底往上（对应数组的从后往前）抽取元素,其中lst[0]必然是堆中最大元素，每次都将lst[i]和lst[0]位置互换，接着再重建大顶堆
+    for i in range(n, 0, -1):
+        # lst[0]
+        lst[i], lst[0] = lst[0], lst[i]
+        # 重建大顶堆
+        max_heap(lst, i, 0)
+
+lst = [ 12, 11, 13, 5, 6, 7]
+heap_sort(lst)
+print(lst)
