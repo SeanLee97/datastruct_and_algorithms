0d59dfd597d41b5a62951f2d3a1467f1bdc18209
Sean <xmlee97@gmail.com>
2018-08-26 17:49:51 +0800

    Update generate_parentheses.py

--- dfs_bfs/generate_parentheses.py
+++ dfs_bfs/generate_parentheses.py
@@ -9,23 +9,23 @@ For example, given n = 3, a solution set is: "((()))", "(()())", "(())()", "()((
 def dfs(left, right, tmp, res):
     if left == 0 and right == 0:
         res.append(tmp)
-        return 
+        return
     # 如果左边还有则继续放
     if left > 0:
-        ntmp = tmp + '('
-        dfs(left-1, right, ntmp, res)
-        tmp = ntmp[:-1] # 回溯
-
+        tmp += "("
+        dfs(left-1, right, tmp, res)
+        tmp = tmp[:-1]
     # 如果左边<右边则放右边
     if left < right:
-        ntmp = tmp + ')'
-        dfs(left, right-1, ntmp, res)
-        tmp = ntmp[:-1] # 回溯
+        tmp += ")"
+        dfs(left, right-1, tmp, res)
+        tmp = tmp[:-1]
 
 def solution(n):
-    tmp = ''
+    tmp = ""
     res = []
     dfs(n, n, tmp, res)
-    print(res)
+    return res
 
-solution(3)  
+if __name__ == '__main__':
+    print(solution(3))
