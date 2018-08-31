cd1a7836eaee41d57d24bb56c95b6906a1ccda5c
SeanLee97 <mitree@sina.com>
2018-08-31 16:40:33 +0800

    update

--- LRU/lru.py
+++ LRU/lru.py
@@ -6,36 +6,39 @@ Description:
 """
 
 class LRUCache(object):
-    def __init__(self, capacity=2):
+    def __init__(self, capacity=3):
         self.capacity = capacity
-        self.store = {}
         self.key = []
+        self.store = {}
 
     def put(self, k, v):
-        if k in self.store:
-            self.key.remove(k)
-            self.key = [k] + self.key
-            self.store[k] = v
-        elif len(self.store.keys()) == self.capacity:
-            last_key = self.key.pop()
-            del self.store[last_key]
-            self.key = [k] + self.key
-            self.store[k] = v
-        else:
-            self.store[k] = v
-            self.key = [k] + self.key
+        if k in self.key:
+            self.key.remove(k)            
+        elif len(self.store) == self.capacity:
+            del self.store[self.key.pop()]
+
+        self.key = [k] + self.key
+        self.store[k] = v
+
     def get(self, k):
         if k in self.store:
             self.key.remove(k)
             self.key = [k] + self.key
             return self.store[k]
+        return None
 
-        return False
+    def __setitem__(self, k, v):
+        self.put(k, v)
+
+    def __getitem__(self, k):
+        return self.get(k)
 
 if __name__ == '__main__':
-    cache = LRUCache(3)
-    cache.put(1, 2)
-    cache.put(2, 3)
-    cache.put(3, 6)
-    print(cache.get(2))
-    print(cache.get(1))
+    cache = LRUCache()
+    cache.put('a', '1')
+    cache.put('b', '2')
+    cache.put('c', '3')
+    cache.put('d', '4')
+    cache['d'] = 5
+    print(cache.get('a'))
+    print(cache['d'])
