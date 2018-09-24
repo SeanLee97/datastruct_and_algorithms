e2de791cde50dcfeac5edeb7555eac74e59bc83d
SeanLee97 <mitree@sina.com>
2018-09-24 19:05:08 +0800

    fixed

--- tree/find_post_order_by_pre_in_order.py
+++ tree/find_post_order_by_pre_in_order.py
@@ -1,42 +1,37 @@
 # -*- coding:utf-8 -*-
 
-class Node(object):
-    def __init__(self, data=None, left=None, right=None):
-        self.data = data
+"""根据先序遍历和中序遍历得到后序遍历，方法：重构树+后序遍历
+"""
+
+class TreeNode(object):
+    def __init__(self, val, left=None, right=None):
+        self.val = val
         self.left = left
         self.right = right
 
-def echo(root):
-    if root == None:
+def build(pre_order, in_order):
+    if len(pre_order) == 0:
+        return None
+    root = TreeNode(pre_order[0])
+    mid = in_order.index(pre_order[0])
+    root.left = build(pre_order[1: mid+1], in_order[:mid])
+    root.right = build(pre_order[mid+1:], in_order[mid+1:])
+    return root
+
+def postorder(pRoot):
+    if pRoot is None:
         return
-    echo(root.left)
-    echo(root.right)
-    print(root.data)
+    postorder(pRoot.left)
+    postorder(pRoot.right)
+    print(pRoot.val)
 
-def build_tree(preorder, inorder, root):
-    curr = preorder.pop(0)
-    root.data = curr 
-    try:
-        idx = inorder.index(curr)
-        l = inorder[:idx]
-        r = inorder[idx:]
-        #print(l, '##', r)
-        if len(l) > 0 and len(set(preorder) & set(l)) > 0:
-            root.left = Node()
-            build_tree(preorder, l, root.left)
-        if len(r) > 0 and len(set(preorder) & set(r)) > 0:
-            root.right = Node()
-            build_tree(preorder, r, root.right)
-    except:
-        pass
-    return
+def rebuild(pre_order, in_order):
+    assert len(pre_order) == len(in_order)
+    root = build(pre_order, in_order)
+    postorder(root)
+    
 
-def get_post(preorder, inorder):
-    root = Node()
-    tree = build_tree(preorder, inorder, root)
-    # 输出 
-    echo(root)
- 
-preorder = [4, 1, 2, 3, 5]
-inorder = [2, 1, 3, 4, 5]
-get_post(preorder, inorder)
+if __name__ == '__main__':
+    pre_order = [1, 2, 4, 5, 3]
+    in_order = [4, 2, 5, 1, 3]
+    rebuild(pre_order, in_order)
